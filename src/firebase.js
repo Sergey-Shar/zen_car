8b924d41cb3e4b883495bb767dd5a435d3892b8a
Sergey-Sh <sergeihar82@gmail.com>
2022-02-06 12:55:58 +0300

    'I make an intermediate commit, the application works correctly'

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAi_PlRCV8atR8d7YOJMMjdhq2KgFXUqwA",
+  authDomain: "zen-car-test.firebaseapp.com",
+  projectId: "zen-car-test",
+  storageBucket: "zen-car-test.appspot.com",
+  messagingSenderId: "1031876127881",
+  appId: "1:1031876127881:web:79d9ac599af669dee56a58"
 };
 
 const app = initializeApp(firebaseConfig);
